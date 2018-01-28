0d0a6f2ea41a2180376a39f77f097a9de9c9df1e
Francis <francisacheampong@gmail.com>
2018-01-27 21:11:39 -0500

    update gamets

--- Scripts/core/game.ts
+++ Scripts/core/game.ts
@@ -1,27 +1,41 @@
-{ 
-    
-}
-
 //IIFE - Immediately Invoked Function Expression
 (function(){
- function Init(){
 
- console.log("Initialization Started")
+    //Game variables 
+    let canvas = document.getElementById("canvas")
+    let stage = createjs.Stage;
+    let helloLabel:createjs.Text;
 
- let myVariable:string = "foo"
- const myVariable1 =0;
- let myVariable_n =0;
- 
- }
- function Start(){
-     
-}
-function Update(){
-     
-}
-function Main(){
-     
-}
+    function Init():void {
 
-window.onload =Init;
+     console.log("Initialization Started");
+     Start();
+
+    }
+    function Start():void {
+        console.log("Starting Application ...");
+
+        stage = new createjs.Stage(canvas);
+        createjs.Ticker.framerate =60; // 60 frames per second
+        createjs.Ticker.on("tick",Update)
+
+        Main();
+    }
+    function Update():void{
+     helloLabel.rotation-= 5;
+     stage.update() // redraws the stage
+    }
+    function Main():void{
+
+    console.log("Game started ...");
+     helloLabel = new createjs.Text("Hello, World!","50px Consolas","#000000")
+     helloLabel.x =100;
+     helloLabel.y=100;
+
+    stage.addChild(helloLabel);
+    
+    }
+
+    window.onload =Init;
+
 })();
