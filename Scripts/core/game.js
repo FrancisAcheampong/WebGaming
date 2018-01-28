70496dd1101d3ac07586f353c7f62b4810864c2e
Francis <francisacheampong@gmail.com>
2018-01-27 20:51:53 -0500

    added functions

---
+++ Scripts/core/game.js
@@ -0,0 +1,19 @@
+{
+}
+//IIFE - Immediately Invoked Function Expression
+(function () {
+    function Init() {
+        console.log("Initialization Started");
+        var myVariable = "foo";
+        var myVariable1 = 0;
+        var myVariable_n = 0;
+    }
+    function Start() {
+    }
+    function Update() {
+    }
+    function Main() {
+    }
+    window.onload = Init;
+})();
+//# sourceMappingURL=game.js.map
---
+++ Scripts/core/game.ts
@@ -0,0 +1,27 @@
+{ 
+    
+}
+
+//IIFE - Immediately Invoked Function Expression
+(function(){
+ function Init(){
+
+ console.log("Initialization Started")
+
+ let myVariable:string = "foo"
+ const myVariable1 =0;
+ let myVariable_n =0;
+ 
+ }
+ function Start(){
+     
+}
+function Update(){
+     
+}
+function Main(){
+     
+}
+
+window.onload =Init;
+})();
